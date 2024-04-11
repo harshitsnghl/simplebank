6b6f5ba3fcffd49b6e2b106a6ecd2a3bd89e31d5
Harshit Singhal <harshitsnghl@gmail.com>
2024-04-12 04:23:59 +0530

    Add users table with unique & foreign key constraints in PostgresSQL

---
+++ util/currency.go
@@ -0,0 +1,19 @@
+package util
+
+// Constants for all supported currencies
+const (
+	USD = "USD"
+	EUR = "EUR"
+	CAD = "CAD"
+	INR = "INR"
+)
+
+// IsSupportedCurrency returns if the currency is supported
+
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case USD, EUR, CAD:
+		return true
+	}
+	return false
+}
